e75ec09a0d24a6db13bd4fd5913436efe76c53cd
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-11-04 07:37:27 -0700

    INFRA: Initialize API Project

---
+++ Sheenam.Api/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿//===================================================
+// Copyright (c)  coalition of Good-Hearted Engineers
+// Free To Use Comfort and Pease
+//===================================================
+
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Sheenam.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("Hello Mario, the princess is in another castle");
+    }
+}
---
+++ Sheenam.Api/Program.cs
@@ -0,0 +1,25 @@
+//===================================================
+// Copyright (c)  coalition of Good-Hearted Engineers
+// Free To Use Comfort and Pease
+//===================================================
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Sheenam.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args) =>
+           CreateHostBuilder(args).Build().Run();
+
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                  .ConfigureWebHostDefaults(webBuilder =>
+                      webBuilder.UseStartup<Startup>());
+        }
+
+    }
+}
