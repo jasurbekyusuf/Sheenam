8a7c23e7d82caa0f760d434a1b479535600767db
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-11-04 22:51:21 -0700

    BROKERS: Insert Guest

---
+++ Sheenam.Api/Brokers/Storages/IStorageBroker.cs
@@ -0,0 +1,12 @@
+﻿//===================================================
+// Copyright (c)  coalition of Good-Hearted Engineers
+// Free To Use Comfort and Pease
+//===================================================
+
+
+namespace Sheenam.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+    }
+}
--- Sheenam.Api/Brokers/Storages/StorageBroker.cs
+++ Sheenam.Api/Brokers/Storages/StorageBroker.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Configuration;
 
 namespace Sheenam.Api.Brokers.Storages
 {
-    public partial class StorageBroker : EFxceptionsContext
+    public partial class StorageBroker : EFxceptionsContext , IStorageBroker
     {
         private readonly IConfiguration configuration;
         public StorageBroker(IConfiguration configuration)
