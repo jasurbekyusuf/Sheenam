483238db3bdce4a2b41fe3833e7fb7e53068cc21
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-01 01:50:45 -0800

    ShouldThrowValidationExceptionOnAddIfGenderIsInvalidAndLogItAsync -> PASS

--- Sheenam.Api/Services/Foundations/Guests/GuestService.Validations.cs
+++ Sheenam.Api/Services/Foundations/Guests/GuestService.Validations.cs
@@ -5,6 +5,7 @@
 
 using System;
 using System.Data;
+using System.Reflection.Metadata;
 using Sheenam.Api.Models.Foundations.Guests;
 using Sheenam.Api.Models.Foundations.Guests.Exceptions;
 
@@ -24,7 +25,8 @@ namespace Sheenam.Api.Services.Foundations.Guests
                     (Rule: IsInvalid(guest.LastName), Parameter: nameof(Guest.LastName)),
                     (Rule: IsInvalid(guest.DateOfBirth), Parameter: nameof(Guest.DateOfBirth)),
                     (Rule: IsInvalid(guest.Email), Parameter: nameof(Guest.Email)),
-                    (Rule: IsInvalid(guest.Address), Parameter: nameof(Guest.Address)));             
+                    (Rule: IsInvalid(guest.Address), Parameter: nameof(Guest.Address)),    
+                    (Rule: IsInvalid(guest.Gender), Parameter: nameof(Guest.Gender)));
         }
 
         private void ValidateGuestNotNull(Guest guest)
@@ -50,6 +52,12 @@ namespace Sheenam.Api.Services.Foundations.Guests
         {
             Condition = date == default,
             Message = "Date is required"
+        };
+
+        private static dynamic IsInvalid(GenderType gender) => new
+        {
+            Condition = Enum.IsDefined(gender) is false,
+            Message = "Value is invalid"
         };
 
         private static void Validate(params (dynamic Rule, string Parametr)[] validations)
