9806a43e13cfe4a0737b106d58272d796b030518
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-23 03:28:17 -0800

    DATA: Model and Migrate Hosts

---
+++ Sheenam.Api/Models/Foundations/Hosts/Host.cs
@@ -0,0 +1,21 @@
+﻿//===================================================
+// Copyright (c)  coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Pease
+//===================================================
+
+using System;
+
+namespace Sheenam.Api.Models.Foundations.Hosts
+{
+    public class Host
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTimeOffset DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public GenderType Gender { get; set; }
+    }
+}
