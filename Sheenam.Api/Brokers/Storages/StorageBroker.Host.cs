a4597fc748a4ba67cc990acf2d96a16ed056fccc
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-23 03:54:47 -0800

    DATA: Model And Migrate Homes

---
+++ Sheenam.Api/Brokers/Storages/StorageBroker.Host.cs
@@ -0,0 +1,15 @@
+﻿//===================================================
+// Copyright (c)  coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Pease
+//===================================================
+
+using Microsoft.EntityFrameworkCore;
+using Sheenam.Api.Models.Foundations.Hosts;
+
+namespace Sheenam.Api.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Host> Hosts { get; set; }
+    }
+}
---
+++ Sheenam.Api/Models/Foundations/Homes/Home.cs
@@ -0,0 +1,25 @@
+﻿//===================================================
+// Copyright (c)  coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Pease
+//===================================================
+
+using System;
+
+namespace Sheenam.Api.Models.Foundations.Homes
+{
+    public class Home
+    {
+        public Guid Id { get; set; }
+        public Guid HostId { get; set; }
+        public string Adress { get; set; }
+        public string AdditionalInfo { get; set; }
+        public bool IsVacant { get; set; }
+        public bool IsPetAllowed { get; set; }
+        public bool IsShared { get; set; }
+        public int NumberOfBedrooms { get; set; }
+        public int NumberOfBathrooms { get; set; }
+        public double Area { get; set; }
+        public decimal Price { get; set; }
+        public HouseType Type { get; set; }
+    }
+}
