839f76aba60ecbb9761ac343ac4ace2915dc0748
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-23 06:18:53 -0800

    BROKERS: Select All Guests

--- Sheenam.Api/Brokers/Storages/StorageBroker.Guests.cs
+++ Sheenam.Api/Brokers/Storages/StorageBroker.Guests.cs
@@ -3,9 +3,11 @@
 // Free To Use To Find Comfort and Pease
 //===================================================
 
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Sheenam.Api.Models.Foundations.Guests;
+using Sheenam.Api.Models.Foundations.Homes;
 
 namespace Sheenam.Api.Brokers.Storages
 {
@@ -15,5 +17,8 @@ namespace Sheenam.Api.Brokers.Storages
 
         public async ValueTask<Guest> InsertGuestAsync(Guest guest) =>
             await InsertAsync(guest);
+
+        public IQueryable<Guest> SelectAllGuests() =>
+            SelectAll<Guest>();
     }
 }
