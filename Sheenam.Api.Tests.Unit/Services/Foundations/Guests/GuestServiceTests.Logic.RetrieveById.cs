1a331166c2b6ed1358e6aa3a168e1b5a0575a088
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-24 00:23:21 -0800

    ShouldRetrieveGuestByIdAsync - FAIL

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Logic.RetrieveById.cs
@@ -0,0 +1,42 @@
+﻿//===================================================
+// Copyright (c)  coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Pease
+//===================================================
+
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Sheenam.Api.Models.Foundations.Guests;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveGuestByIdAsync()
+        {
+            // given
+            Guid randomGuestId = Guid.NewGuid();
+            Guid inputGuestId = randomGuestId;
+            Guest randomGuest = CreateRandomGuest();
+            Guest storageGuest = randomGuest;
+            Guest expectedGuest = storageGuest.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectGuestByIdAsync(inputGuestId)).ReturnsAsync(storageGuest);
+
+            // when
+            Guest actualGuest =
+                await this.guestService.RetrieveGuestByIdAsync(inputGuestId);
+
+            // then
+            actualGuest.Should().BeEquivalentTo(expectedGuest);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectGuestByIdAsync(inputGuestId), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- Sheenam.Api/Services/Foundations/Guests/GuestService.cs
+++ Sheenam.Api/Services/Foundations/Guests/GuestService.cs
@@ -3,6 +3,7 @@
 // Free To Use To Find Comfort and Pease
 //===================================================
 
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Sheenam.Api.Brokers.Loggings;
@@ -35,5 +36,9 @@ namespace Sheenam.Api.Services.Foundations.Guests
         public IQueryable<Guest> RetrieveAllGuests() =>
         TryCatch(() => this.storageBroker.SelectAllGuests());
 
+        public ValueTask<Guest> RetrieveGuestByIdAsync(Guid guestId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
