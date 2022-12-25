ea87f20e0b6ae136a49d6e689f71bc792be86e72
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-25 05:58:12 -0800

    ShouldRemoveGuestByIdAsync -> PASS

--- Sheenam.Api/Services/Foundations/Guests/GuestService.cs
+++ Sheenam.Api/Services/Foundations/Guests/GuestService.cs
@@ -64,9 +64,12 @@ namespace Sheenam.Api.Services.Foundations.Guests
             return await this.storageBroker.UpdateGuestAsync(guest);
         });
 
-        public ValueTask<Guest> RemoveGuestByIdAsync(Guid guestId)
+        public async ValueTask<Guest> RemoveGuestByIdAsync(Guid guestId)
         {
-            throw new NotImplementedException();
+            Guest maybeGuest =
+                await this.storageBroker.SelectGuestByIdAsync(guestId);
+
+            return await this.storageBroker.DeleteGuestAsync(maybeGuest);
         }
     }
 }
