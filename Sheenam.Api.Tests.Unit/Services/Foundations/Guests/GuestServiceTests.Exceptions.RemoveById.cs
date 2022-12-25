7f71f314388ec0c378bd05f949608bea25595110
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-25 07:08:27 -0800

    ShouldThrowDependencyExceptionOnDeleteWhenSqlExceptionOccursAndLogItAsync -> PASS

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Exceptions.RemoveById.cs
@@ -0,0 +1,57 @@
+﻿//===================================================
+// Copyright (c)  coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Pease
+//===================================================
+
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Sheenam.Api.Models.Foundations.Guests;
+using Sheenam.Api.Models.Foundations.Guests.Exceptions;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnDeleteWhenSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            Guid someGuestId = Guid.NewGuid();
+            SqlException sqlException = CreateSqlException();
+
+            var failedGuestStorageException =
+                new FailedGuestStorageException(sqlException);
+
+            var expectedGuestDependencyException =
+                new GuestDependencyException(failedGuestStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectGuestByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<Guest> deleteGuestTask =
+                this.guestService.RemoveGuestByIdAsync(someGuestId);
+
+            GuestDependencyException actualGuestDependencyException =
+                await Assert.ThrowsAsync<GuestDependencyException>(
+                    deleteGuestTask.AsTask);
+
+            // then
+            actualGuestDependencyException.Should().BeEquivalentTo(
+                expectedGuestDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectGuestByIdAsync(It.IsAny<Guid>()), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedGuestDependencyException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
