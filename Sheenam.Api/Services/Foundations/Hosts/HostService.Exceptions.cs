a63c4447c75cd8b764437a3ab608ed44720b582e
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-27 07:57:23 -0800

    ShouldThrowDependencyValidationExceptionOnAddIfDbConcurrencyErrorOccursAndLogItAsync -> PASS

--- Sheenam.Api/Services/Foundations/Hosts/HostService.Exceptions.cs
+++ Sheenam.Api/Services/Foundations/Hosts/HostService.Exceptions.cs
@@ -6,6 +6,7 @@
 using System.Threading.Tasks;
 using EFxceptions.Models.Exceptions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Sheenam.Api.Models.Foundations.Hosts;
 using Sheenam.Api.Models.Foundations.Hosts.Exceptions;
 using Xeptions;
@@ -42,6 +43,12 @@ namespace Sheenam.Api.Services.Foundations.Hosts
                      new AlreadyExistsHostException(duplicateKeyException);
 
                 throw CreateAndDependencyValidationException(alreadyExistsHostException);
+            }
+            catch (DbUpdateConcurrencyException dbUpdateConcurrencyException)
+            {
+                var lockedHostException = new LockedHostException(dbUpdateConcurrencyException);
+
+                throw CreateAndDependencyValidationException(lockedHostException);
             }
         }
 
