90f95084bdb5183bed667fcd23a925fa68665228
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-27 03:40:04 -0800

    ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync -> WIP

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Hosts/HostServiceTests.Validations.Add.cs
@@ -0,0 +1,16 @@
+﻿//===================================================
+// Copyright (c)  coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Pease
+//===================================================
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Hosts
+{
+    public partial class HostServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfInputIsNullAndLogItAsync()
+        {
+
+        }
+    }
+}
