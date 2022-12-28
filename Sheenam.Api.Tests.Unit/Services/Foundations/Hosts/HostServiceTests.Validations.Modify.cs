6c967573825120ee646de1e73511360e5b1e4acc
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-28 06:49:24 -0800

    ShouldThrowValidationExceptionOnModifyIfHostIsNullAndLogItAsync -> FAIL

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Hosts/HostServiceTests.Validations.Modify.cs
@@ -0,0 +1,46 @@
+﻿//===================================================
+// Copyright (c)  coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Pease
+//===================================================
+
+using FluentAssertions;
+using Moq;
+using Sheenam.Api.Models.Foundations.Hosts;
+using Sheenam.Api.Models.Foundations.Hosts.Exceptions;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Hosts
+{
+    public partial class HostServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfHostIsNullAndLogItAsync()
+        {
+            //given
+            Host nullHost = null;
+            var nullHostException = new NullHostException();
+
+            var expectedHostValidationException =
+                new HostValidationException(nullHostException);
+
+            //when
+            ValueTask<Host> modifyHostTask =
+                this.hostService.ModifyHostAsync(nullHost);
+
+            HostValidationException actualHostValidationException =
+                await Assert.ThrowsAsync<HostValidationException>(modifyHostTask.AsTask);
+
+            //then
+            actualHostValidationException.Should().BeEquivalentTo(expectedHostValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedHostValidationException))), Times.Once());
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateHostAsync(It.IsAny<Host>()), Times.Never());
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
