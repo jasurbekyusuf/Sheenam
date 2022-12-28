bdd2614d2f645077909bdf3217a821cb086fb644
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-28 07:21:55 -0800

    ShouldThrowValidationExceptionOnModifyIfHostIsInvalidAndLogItAsync -> FAIL

--- Sheenam.Api.Tests.Unit/Services/Foundations/Hosts/HostServiceTests.Validations.Modify.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Hosts/HostServiceTests.Validations.Modify.cs
@@ -42,5 +42,91 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Hosts
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfHostIsInvalidAndLogItAsync(string invalidString)
+        {
+            // given 
+            var invalidHost = new Host
+            {
+                FirstName = invalidString
+            };
+
+            var invalidHostException = new InvalidHostException();
+
+            invalidHostException.AddData(
+                key: nameof(Host.Id),
+                values: "Id is required");
+
+            invalidHostException.AddData(
+                key: nameof(Host.FirstName),
+                values: "Text is required");
+
+            invalidHostException.AddData(
+                key: nameof(Host.LastName),
+                values: "Text is required");
+
+            invalidHostException.AddData(
+                key: nameof(Host.DateOfBirth),
+                values: "Date is required");
+
+            invalidHostException.AddData(
+                key: nameof(Host.Email),
+                values: "Text is required");
+
+            invalidHostException.AddData(
+                key: nameof(Host.PhoneNumber),
+                values: "Text is required");
+
+            invalidHostException.AddData(
+               key: nameof(Host.CreatedDate),
+               values: "Date is required");
+
+            invalidHostException.AddData(
+                key: nameof(Host.UpdatedDate),
+                    values: new[]
+                    {
+                        "Date is required",
+                        "Date is not recent",
+                        $"Date is the same as {nameof(Host.CreatedDate)}"
+                    }
+                );
+
+            var expectedHostValidationException =
+                new HostValidationException(invalidHostException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTime())
+                    .Returns(GetRandomDateTimeOffset);
+
+            // when
+            ValueTask<Host> modifyHostTask =
+                this.hostService.ModifyHostAsync(invalidHost);
+
+            HostValidationException actualHostValidationException =
+                await Assert.ThrowsAsync<HostValidationException>(
+                    modifyHostTask.AsTask);
+
+            //then
+            actualHostValidationException.Should()
+                .BeEquivalentTo(expectedHostValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedHostValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateHostAsync(It.IsAny<Host>()), Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
