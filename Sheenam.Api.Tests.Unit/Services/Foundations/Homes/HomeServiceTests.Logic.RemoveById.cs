a19ba45d8866d6d207758064c39f3f4d14ccb4ca
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-04-03 07:22:25 +0500

    ShouldRemoveHomeById -> FAIL

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Homes/HomeServiceTests.Logic.RemoveById.cs
@@ -0,0 +1,57 @@
+﻿//===================================================
+// Copyright (c)  coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Pease
+//===================================================
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Sheenam.Api.Models.Foundations.Homes;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Homes
+{
+    public partial class HomeServiceTests
+    {
+        [Fact]
+        public async Task ShouldRemoveHomeById()
+        {
+            // given
+            Guid randomId = Guid.NewGuid();
+            Guid inputHomeId = randomId;
+            Home randomHome = CreateRandomHome();
+            Home storageHome = randomHome;
+            Home expectedInputHome = storageHome;
+            Home deletedHome = expectedInputHome;
+            Home expectedHome = deletedHome.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectHometByIdAsync(inputHomeId))
+                    .ReturnsAsync(storageHome);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.DeleteHomeAsync(expectedInputHome))
+                    .ReturnsAsync(deletedHome);
+
+            // when
+            Home actualHome = await this.homeService
+                .RemoveHomeByIdAsync(inputHomeId);
+
+            // then
+            actualHome.Should().BeEquivalentTo(expectedHome);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectHometByIdAsync(inputHomeId), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteHomeAsync(expectedInputHome), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+
+}
---
+++ Sheenam.Api/Services/Foundations/Homes/HomeService.cs
@@ -0,0 +1,22 @@
+﻿//===================================================
+// Copyright (c)  coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Pease
+//===================================================
+
+using System;
+using System.Threading.Tasks;
+using Sheenam.Api.Brokers.Storages;
+using Sheenam.Api.Models.Foundations.Homes;
+
+namespace Sheenam.Api.Services.Foundations.Homes
+{
+    public class HomeService:IHomeService
+    {
+        private IStorageBroker storageBroker;
+
+        public HomeService(IStorageBroker storageBroker)=>
+            this.storageBroker = storageBroker;
+        public ValueTask<Home> RemoveHomeByIdAsync(Guid guestId)=>
+            throw new NotImplementedException();
+    }
+}
